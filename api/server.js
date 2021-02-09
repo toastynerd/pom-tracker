bc33312963a1521f487965e5e310f670e31753fa
toastynerd <tylermorgan86@gmail.com>
2021-02-08 18:24:15 -0800

    finish docker compose setup

--- api/server.js
+++ api/server.js
@@ -3,8 +3,6 @@ const server = express();
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI);
-db = mongoose.connection;
-db.on('error', (error) => console.log(`error: ${error}`));
 
 server.get('/', (req, res) => {
   res.send('here is the server\n');
